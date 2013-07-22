250e43f6da4959b287c063d56d8adbed9f2a658a
Yoz Grahame <yoz@yoz.com>
2013-07-22 14:21:25 -0700

    Can be started and stopped

--- lib/panorama/tracer.rb
+++ lib/panorama/tracer.rb
@@ -1,5 +1,21 @@
 module Panorama
   class Tracer
 
+    def initialize
+      @active_state = false
+    end
+
+    def active?
+      @active_state
+    end
+
+    def start
+      @active_state = true
+    end
+
+    def stop
+      @active_state = false
+    end
+
   end
 end
--- spec/panorama/tracer_spec.rb
+++ spec/panorama/tracer_spec.rb
@@ -3,7 +3,27 @@ require 'spec_helper'
 describe Panorama::Tracer do
 
   it 'can be instantiated' do
-    expect(described_class.new).to be_a(described_class)
+    expect(described_class.new).to be_a Panorama::Tracer
+  end
+
+  describe "state" do
+    let(:tracer) { Panorama::Tracer.new }
+
+    it 'starts in an inactive state' do
+      expect(tracer).to_not be_active
+    end
+
+    it 'can be started' do
+      tracer.start
+      expect(tracer).to be_active
+    end
+
+    it 'can be stopped' do
+      tracer.start
+      tracer.stop
+      expect(tracer).to_not be_active
+    end
+
   end
 
 end
