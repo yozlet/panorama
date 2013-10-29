fa4bef4b20ace7e157aed30ced9b0d7ca9d8783a
Yoz Grahame <yoz@yoz.com>
2013-10-29 15:16:44 -0700

    Start of file support at Sinatra app level
    Using app settings instead of passed parameters for security bonus!

--- lib/panorama/viewer/app.rb
+++ lib/panorama/viewer/app.rb
@@ -3,8 +3,16 @@ require 'sinatra'
 module Panorama
   class Viewer < Sinatra::Base
 
+    set :code_path, nil
+
     get '/' do 
-      'Hello World!'
+      "Hello World! Code path is #{settings.code_path}"
+    end
+
+    # start the server if ruby file executed directly
+    if app_file == $0
+      set :code_path, $1
+      run!
     end
 
   end
--- spec/panorama/viewer_spec.rb
+++ spec/panorama/viewer_spec.rb
@@ -3,6 +3,7 @@ ENV['RACK_ENV'] = 'test'
 require 'spec_helper'
 require 'rack/test'
 require 'panorama/viewer/app.rb'
+require 'Open3'
 
 describe Panorama::Viewer do
   include Rack::Test::Methods
@@ -11,9 +12,34 @@ describe Panorama::Viewer do
     Panorama::Viewer
   end
 
+  let(:code_path) { "../spec/fixtures/hello.rb" }
+
   it "says hello" do
     get '/'
     expect(last_response).to be_ok
-    expect(last_response.body).to eql('Hello World!')
+    expect(last_response.body).to match('Hello World!')
+  end
+
+  it 'can be run from the command line' do
+    pending
+    arg = code_path
+    exe = 'ruby'
+    appfile = File.expand_path('../../lib/panorama/viewer/app.rb', File.dirname(__FILE__))
+    Open3.popen3("#{exe} #{appfile} #{arg}") do |stdin, stdout, stderr, wait_thr|
+      stdin.close
+      while line = stderr.gets
+        puts line
+        if line =~ /start/
+          break
+        end
+      end
+    end
+  end
+
+  it "accepts a path to a ruby file to run" do
+    app.settings.code_path = code_path
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(code_path)
   end
 end
